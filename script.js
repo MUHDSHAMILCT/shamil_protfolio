ca765e1ee69d492d8fd9058b667df1d7ff2e3f4d
muhd shamil ct <muhdshamilct11@gmail.com>
2025-08-08 10:13:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Simple form submission handler
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thanks for your message!');
+});
+function namee(){
+    alert("enter name")
+}
+function submit(){
+  const nam=document.getElementById("sbmt");
+  const pop=nam.
+}
